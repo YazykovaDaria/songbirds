39893dabccc5ceacf28f39dfef0a9739df00b911
YazykovaDaria <yazykovadasva@gmail.com>
2025-01-28 21:35:47 +0300

    feat: partials structure

---
+++ src/locales/en.js
@@ -0,0 +1,7 @@
+const en = {
+  quizzName: 'Song Birds Quiz',
+  tryGuess: 'Try to guess the bird by its song',
+  start: 'Start'
+}
+
+export default en;
---
+++ src/locales/ru.js
@@ -0,0 +1,7 @@
+const ru = {
+  quizzName: 'Викторина песни птиц',
+  tryGuess: 'Попробуй угадать птицу по звукам её песни',
+  start: 'Начать'
+}
+
+export default ru;
