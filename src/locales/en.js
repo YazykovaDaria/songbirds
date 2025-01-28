fc6bd086fed32250a3f46d0b8de3a1ec8ff4b997
YazykovaDaria <yazykovadasva@gmail.com>
2025-01-28 22:58:14 +0300

    feat: quiz panel

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,7 +1,16 @@
 const en = {
   quizzName: 'Song Birds Quiz',
   tryGuess: 'Try to guess the bird by its song',
-  start: 'Start'
+  start: 'Start',
+  panel: {
+    warmUp: 'Warm-up',
+    singing: 'Singing Birds',
+    sparrows: 'Sparrows',
+    forest: 'Forest Birds',
+    prey: 'Birds of Prey',
+    seabirds: 'Seabirds',
+    score: 'Score:'
+  }
 }
 
 export default en;
